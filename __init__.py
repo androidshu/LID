5b9a341a71a8b7ee17049b15d72352d25826902f
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-06-21 17:45:42 +0800

    add init.py file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+from .audio_lid import AudioLID
+from .language_identify import LanguageIdentify
+from .speech_detecting import SpeechDetecting, SpeechDetectObject
+from .error_codes import *
+
+
+__all__ = ['MyModuleClass']
