5911edd878614f6f4b52109406e0ebd79fe266c4
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-06-21 17:48:59 +0800

    add real class name

--- __init__.py
+++ __init__.py
@@ -6,4 +6,4 @@ from .speech_detecting import SpeechDetecting, SpeechDetectObject
 from .error_codes import *
 
 
-__all__ = ['MyModuleClass']
+__all__ = ['AudioLID', 'LanguageIdentify', 'SpeechDetecting', 'SpeechDetectObject']
