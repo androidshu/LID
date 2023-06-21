75d8e9e41dbde43e8e24bd5a6f069967c02bd9b5
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-06-21 16:37:55 +0800

    add setup.py for pip installing

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LID',
+    version='0.1',
+    description='A project that detect the language from a audio file',
+    author='bevis',
+    author_email='javashu2012@gmail.com',
+    url='https://github.com/androidshu/LID.git',
+    packages=find_packages(),
+    install_requires=[
+        'git+https://github.com/facebookresearch/fairseq.git@a29952ce6d313a4daf3e90647f8bf84cc6d4df6d#egg=fairseq',
+        'pytest-runner==6.0.0',
+        'paddlespeech',
+        'paddlepaddle==2.4.1',
+        'scipy',
+        'tqdm',
+        'Pillow',
+        'torch>=1.10.0'
+    ]
+)
