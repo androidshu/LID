a128750e90aa53d62273ba75f9d0977b51f68fac
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-06-25 10:55:51 +0800

    add import file in init.py

--- audio_lid/__init__.py
+++ audio_lid/__init__.py
@@ -5,5 +5,8 @@ from .language_identify import LanguageIdentify
 from .speech_detecting import SpeechDetecting, SpeechDetectObject
 from .error_codes import *
 
+import audio_lid.language_identify
+import audio_lid.speech_detecting
+import audio_lid.error_codes
 
 __all__ = ['AudioLID', 'LanguageIdentify', 'SpeechDetecting', 'SpeechDetectObject']
