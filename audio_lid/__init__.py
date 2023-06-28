c827587254cee244d5a8124e3f7b57a2e07362ba
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-06-28 16:02:01 +0800

    add run module

--- audio_lid/__init__.py
+++ audio_lid/__init__.py
@@ -2,6 +2,7 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from .audio_lid import AudioLID
+from .audio_lid import run
 from .language_identify import LanguageIdentify
 from .speech_detecting import SpeechDetecting, SpeechDetectObject
 from .error_codes import *
@@ -10,4 +11,4 @@ import audio_lid.language_identify
 import audio_lid.speech_detecting
 import audio_lid.error_codes
 
-__all__ = ['AudioLID', 'LanguageIdentify', 'SpeechDetecting', 'SpeechDetectObject']
+__all__ = ['AudioLID', 'LanguageIdentify', 'SpeechDetecting', 'SpeechDetectObject', 'run']
