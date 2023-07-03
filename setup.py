f771066d68fcd5dc204aa26e759868ffc27f4d28
bevis <xuhuishu.xhs@alibaba-inc.com>
2023-07-03 10:46:06 +0800

    split requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/androidshu/audio-lid.git',
     packages=find_packages(),
     install_requires=[
-        'fairseq@git+https://github.com/facebookresearch/fairseq.git@a29952ce6d313a4daf3e90647f8bf84cc6d4df6d',
+        #'fairseq@git+https://github.com/facebookresearch/fairseq.git@a29952ce6d313a4daf3e90647f8bf84cc6d4df6d',
         'pytest-runner==6.0.0',
         'paddlespeech',
         'paddlepaddle==2.4.1',
